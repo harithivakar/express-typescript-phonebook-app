cde9180506fb29eb2e35a4eb23ff9d849cd3d86a
Harithivakar <harithivakar.nadar@gmail.com>
2022-07-12 21:15:45 +0530

    defined api routes

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,3 +1,5 @@
 export default class Constants{
-    public static readonly CONTEXT_PATH = "/api";
+    public static readonly DOMAIN = "/phonebook";
+    public static readonly CONTEXT_PATH = `${Constants.DOMAIN}/api`;
+    public static readonly DOCS_PATH = `${Constants.DOMAIN}/api/docs`;
 }
---
+++ src/controller/ContactController.ts
@@ -0,0 +1,7 @@
+import { controller } from "inversify-express-utils";
+import Constants from "../constants/Constants";
+
+@controller(`${Constants.CONTEXT_PATH}/contacts`)
+export class ContactController {
+
+}
